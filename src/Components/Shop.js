336585d5deadcb0f0c6b1725163ba46e4cb6447f
Shreya <shreya.sati@kiwitech.com>
2023-02-13 19:43:32 +0530

    updating state from different component

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../State';
+import { bindActionCreators } from 'redux';
 
 function Shop() {
+  const dispatch =useDispatch();
+  const actions = bindActionCreators(actionCreators,dispatch);
   return (
     <div>
         <h2>Buy Adidas Shoes 5000rs</h2>
-        <button className='btn btn-primary'>-</button>
+        <button className='btn btn-primary mx-2' onClick={()=>{actions.withdrawMoney(100)}}>-</button>
          Add this item to cart
-        <button className='btn btn-primary'>+</button>
+        <button className='btn btn-primary mx-2' onClick={()=>{actions.depositMoney(100)}}>+</button>
+       
     </div>
   )
 }
