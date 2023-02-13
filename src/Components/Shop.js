ffec9bbd6a55e0f29e41663c31327d01483addd3
Shreya <shreya.sati@kiwitech.com>
2023-02-13 19:00:58 +0530

    Redux setup Action Reducer and Store

---
+++ src/Components/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Shop() {
+  return (
+    <div>
+        <h2>Buy Adidas Shoes 5000rs</h2>
+        <button className='btn btn-primary'>-</button>
+         Add this item to cart
+        <button className='btn btn-primary'>+</button>
+    </div>
+  )
+}
+export default Shop;
+
+
+
